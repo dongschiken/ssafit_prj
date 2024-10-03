233a1b5ffd62dc5b15c6a882e9aae26a75890b34
dongs <138917930+dongschiken@users.noreply.github.com>
2024-10-04 08:23:02 +0900

    myboard list 기능 추가

--- src/main/java/com/cdu/ssafit/member/model/service/MemberServiceImpl.java
+++ src/main/java/com/cdu/ssafit/member/model/service/MemberServiceImpl.java
@@ -3,6 +3,7 @@ package com.cdu.ssafit.member.model.service;
 import java.sql.SQLException;
 import java.util.Map;
 
+import com.cdu.ssafit.board.domain.dto.Board;
 import com.cdu.ssafit.member.domain.dto.Member;
 import com.cdu.ssafit.member.domain.dto.Review;
 import com.cdu.ssafit.member.model.repository.MemberRepository;
@@ -55,5 +56,17 @@ public class MemberServiceImpl implements MemberService {
 	@Override
 	public void deleteReview(int id) throws SQLException {
 		memberRepository.deleteReview(id);
+	}
+
+	@Override
+	public Map<Integer, Board> selectBoardList(String option, Member member) throws SQLException {
+		if (option == null || option.equals("newest")) {
+			option = "DESC";
+		} else if (option.equals("oldest")) {
+			option = "ASC";
+		} else {
+
+		}
+		return memberRepository.selectBoardList(option, member);
 	}
 }
